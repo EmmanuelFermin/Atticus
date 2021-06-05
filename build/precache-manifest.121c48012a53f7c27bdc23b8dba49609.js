5cb716825b95a89f7b67c53319be0dfa65d5d8f5
Emmanuel Fermin <emmanuelfermin1990@gmail.com>
2021-06-05 21:58:44 +0800

    TESTING BUILD PUSH

---
+++ build/precache-manifest.121c48012a53f7c27bdc23b8dba49609.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7766cf009f41e7d2e44b33a46f96a832",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1343ac66371e3024c817",
+    "url": "/static/css/main.2287d797.chunk.css"
+  },
+  {
+    "revision": "107f85a42345961b5d15",
+    "url": "/static/js/2.e902803d.chunk.js"
+  },
+  {
+    "revision": "1343ac66371e3024c817",
+    "url": "/static/js/main.b92e4259.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
